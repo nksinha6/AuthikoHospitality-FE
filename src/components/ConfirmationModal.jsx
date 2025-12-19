db8c6fcdd538f402dd6a1875a162781b10a5e9a0
Vickysonaiya <vickysonaiya0189@gmail.com>
2025-12-19 21:15:24 +0530

    error solve

--- src/components/ConfirmationModal.jsx
+++ src/components/ConfirmationModal.jsx
@@ -38,7 +38,7 @@ const ConfirmationModal = ({
                 <div className="p-6">
                     <div className="flex gap-4">
                         {isDanger && (
-                            <div className="flex-shrink-0">
+                            <div className="shrink-0">
                                 <div className="w-10 h-10 bg-red-50 rounded-full flex items-center justify-center">
                                     <AlertTriangle className="w-5 h-5 text-red-600" />
                                 </div>
